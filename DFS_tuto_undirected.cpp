9cab1ddf0e1ceb6b85a1295168f1353360a9c584
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-01-01 10:04:48 +0700

    01 01 23

---
+++ DFS_tuto_undirected.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+typedef long long ll;
+typedef unsigned long long ul;
+typedef long double ld;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<bool, ll> pbl;
+typedef pair<ul, int> pui;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<int> vi;
+typedef vector<int>::iterator ip;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<pll> vpll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pbl> vpbl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<pdd> vpdd;
+typedef vector<string> vs;
+typedef vector<pui> vpui;
+typedef vector<ul> vu;
+typedef vector<string> vs;
+#define mp make_pair
+#define gl getline
+#define pb push_back
+#define eb emplace_back
+#define ob pop_back
+#define sz(s) ((int)(s.size()))
+#define UM uno\nrdered_map
+#define US uno\nrdered_set
+#define forn(i, n) for (int i = 1; i <= int(n); i++)
+#define fora(i, n) for(auto x:n)
+#define Len 100005
+int n,m;
+vector<int> adj[1001];
+bool visited[1001];
+
+//input
+// 9 9
+// 1 2
+// 1 3
+// 1 5
+// 2 4
+// 3 6
+// 3 7
+// 3 9
+// 5 8
+// 8 9
+void inp()
+{
+    cin >> n >> m;
+    for(int i=0; i<m; i++)
+    {
+        int x,y; cin >> x >> y;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    memset(visited, false, sizeof(visited));
+}
+
+// Ngon ngu lap trinh C++
+void dfs(int u)
+{
+    cout << u << " ";
+    visited[u] = true; // danh dau dinh u da duoc tham
+    for(int v : adj[u]) // duyet cac dinh ke voi dinh u
+    {
+        if(!visited[v]) // neu dinh v chua duoc tham
+        {
+            dfs(v); // duyet tiep tu dinh v
+        }
+    }
+}
+
+int main()
+{
+    inp();
+    int k; cin >> k;
+    dfs(k);
+}
+
---
+++ Van_Dijkstra.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+typedef long long ll;
+typedef unsigned long long ul;
+typedef long double ld;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<bool, ll> pbl;
+typedef pair<ul, int> pui;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<int> vi;
+typedef vector<int>::iterator ip;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<pll> vpll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pbl> vpbl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<pdd> vpdd;
+typedef vector<string> vs;
+typedef vector<pui> vpui;
+typedef vector<ul> vu;
+typedef vector<string> vs;
+#define mp make_pair
+#define gl getline
+#define pb push_back
+#define eb emplace_back
+#define ob pop_back
+#define sz(s) ((int)(s.size()))
+#define UM uno\nrdered_map
+#define US uno\nrdered_set
+#define forn(i,a,b) for (int i = (int)a; i < int(b); i++)
+#define forr(i,a,b) for (int i = (int)a; i >= int(b); i--)
+#define fora(i, n) for(auto i:n)
+#define Len 100005
+const double pi=3.14159265358979323846;
+const int MOD = 1000000007;
+#define V 1000
+int minDistance(int dist[], bool sptSet[])
+{
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+    return min_index;
+}
+void printSolution(int dist[], int n)
+{
+    cout << "Vertex Distance from Source" << endl;
+    for (int i = 0; i < V; i++)
+        cout << i << " tt " << dist[i] << endl;
+}
+void Dijkstra(int graph[V][V], int src)
+{
+    int dist[V];
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+    dist[src] = 0;
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+        for (int v = 0; v < V; v++)
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+    printSolution(dist, V);
+}
+
+int main()
+{
+	
+}
